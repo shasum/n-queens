38fcea916f88fa4bcf543e5fd25fcbfc9d067618
Sanjay Chakrapani <csanjay@gmail.com>
2015-08-26 19:54:54 -0400

    Major and Minor Diagonals work now

--- src/Board.js
+++ src/Board.js
@@ -123,12 +123,26 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rows = this.rows();
+      var diag = [];
+      for (var i = 0; i < rows.length; i++) {
+        diag.push(rows[i][majorDiagonalColumnIndexAtFirstRow++]);
+        if (majorDiagonalColumnIndexAtFirstRow >= rows.length) {
+          break;
+        }
+      };
+      var count = _.reduce(diag, function(accumulator, item){
+        return accumulator = accumulator + item;
+      }, 0);
+      return count > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var rows = this.rows();
+      return _.reduce(rows, function(accumulator, item, index){
+        return accumulator = accumulator || this.hasMajorDiagonalConflictAt(index);
+      }.bind(this), false);
     },
 
 
@@ -138,12 +152,26 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rows = this.rows();
+      var diag = [];
+      for (var i = 0; i < rows.length; i++) {
+        diag.push(rows[i][minorDiagonalColumnIndexAtFirstRow--]);
+        if (minorDiagonalColumnIndexAtFirstRow < 0) {
+          break;
+        }
+      };
+      var count = _.reduce(diag, function(accumulator, item){
+        return accumulator = accumulator + item;
+      }, 0);
+      return count > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var rows = this.rows();
+      return _.reduce(rows, function(accumulator, item, index){
+        return accumulator = accumulator || this.hasMinorDiagonalConflictAt(index);
+      }.bind(this), false);
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
